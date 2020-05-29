7785f2ee066365b3870835ae9d4884cf8c82609d
Trevor Sawler <trevor.sawler@gmail.com>
2020-05-29 13:15:54 -0300

    Migrations & models

---
+++ clientmodels/faust_models.go
@@ -0,0 +1,25 @@
+package clientmodels
+
+import (
+	"time"
+)
+
+// FTMember describes a member
+type FTMember struct {
+	ID        int
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// PTMember describes a pt member
+type PTMember struct {
+	ID        int
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type VoteResult struct {
+	ID int
+}
