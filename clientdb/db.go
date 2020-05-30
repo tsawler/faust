11343a712a0895fa559ea995690e6af0647bce12
Trevor Sawler <trevor.sawler@gmail.com>
2020-05-30 15:48:40 -0300

    Voting

--- clientdb/db.go
+++ clientdb/db.go
@@ -150,9 +150,7 @@ func (m *DBModel) GetAllFTMembers() ([]clientmodels.FTMember, error) {
 
 	var members []clientmodels.FTMember
 
-	// TODO: remove where
-
-	query := "select id, first_name, email, voted from ft_members where id = 87 order by id"
+	query := "select id, first_name, email, voted from ft_members order by id"
 
 	rows, err := m.DB.QueryContext(ctx, query)
 	defer rows.Close()
@@ -182,9 +180,7 @@ func (m *DBModel) GetAllPTMembers() ([]clientmodels.PTMember, error) {
 
 	var members []clientmodels.PTMember
 
-	// TODO: remove where
-
-	query := "select id, first_name, email, voted from pt_members where id in (80) order by id"
+	query := "select id, first_name, email, voted from pt_members order by id"
 
 	rows, err := m.DB.QueryContext(ctx, query)
 	defer rows.Close()
